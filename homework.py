c31bbeadf4ed49d6fc586a0256e8a22b5ce5f8c8
IhorFreeMan <greskoihor@gmail.com>
2023-02-28 12:32:51 +0200

    generator 2

--- homework.py
+++ homework.py
@@ -78,3 +78,17 @@ if __name__ == "__main__":
         print(i)
 
     print(MyIterable(l)[2])
+
+
+    def f(value):
+        while True:
+            value = (yield value)
+            a = f(10)
+
+
+        print(next(a))
+        print(next(a))
+        print(a.send(20))
+
+
+
